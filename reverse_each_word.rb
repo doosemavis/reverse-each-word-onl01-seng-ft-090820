a06c848fc12a9217cacbf556d172d803a7c31f2c
Moose Davis <moosedavis2011@gmail.com>
2020-09-13 18:13:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
+sentence = "Hi!  How are you today?"
+
 def reverse_each_word(sentence)
-  new_sentence = sentence.split (" ")
+  new_sentence = sentence.split(" ")
   reverse_sentence = new_sentence.each {|x| x.reverse!}
   return reversed_sentence.join(" ")
   end
