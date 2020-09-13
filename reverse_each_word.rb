4250907c6a53d2227ba792f939db05f4e6a320d4
Moose Davis <moosedavis2011@gmail.com>
2020-09-13 18:12:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split (" ")
   reverse_sentence = new_sentence.each {|x| x.reverse!}
-  sentence.each do |words|
+  return reversed_sentence.join(" ")
     puts words 
   end 
   puts "Hi! How are you today?"
